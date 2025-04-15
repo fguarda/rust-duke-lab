c9b8fe613be3118c6c29c287f7d2a2a13389c9a0
Felipe Guarda <felipe.guarda@proton.me>
2025-04-14 22:27:02 -0300

    Demonstration: Variable Assignment and Immutability

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let mut message= String::from("Name: Felipe Guarda, Weight: ");
+    let mut weight = 85.0;
+
+    let kilos= weight / 2.2;
+    println!("{}{}", message, weight);
 }
