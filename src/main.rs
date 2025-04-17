a50ddbaf8a55640b6419f8a4cf334b00213aee08
Felipe Guarda <felipe.guarda@proton.me>
2025-04-16 22:05:25 -0300

    Demo: The while loop in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-// There are other conditionals that we can explore in Rust. Like using `if let`
+use std::io;
+
+// This example  is a usseful application of `while` because i allows to continue
+// asking for user input until the user types a specific word (in this case, "stop")
 
 fn main () {
-  // let mut maybe_number = Some(None);
-  let maybe_number = Some(42);
-  if  let  Some(number) = maybe_number {
-    println!("The number is {}", number);      
-  } else {
-    println!("There is no number")
-  }
+  let mut input = String::new();
+  while input.trim() != "stop" {
+    input.clear();
+    println!("Please enter a worrd (type 'stop' to exit): ");
+    io::stdin().read_line(&mut input).expect("Failed to read input");
+    println!("You entered: {}", input);
+  } 
+  println!("Goodbye!");
 }
+
+
+// fn main () {
+//   let mut i =0;
+//   while i <= 5 {
+//     println!("i = {}", i);
+//     i += 1;
+//   }
+// }
