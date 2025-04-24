6767e493da7856ae3aaef3684d18d721ec2a0b91
Felipe Guarda <felipe.guarda@proton.me>
2025-04-24 19:26:28 -0300

    Demo: Using arguments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::result;
 
-fn split_string(s: String, delimiter: char, field: usize) -> String {
-  let parts: Vec<&str> = s.split(delimiter).collect();
-  let result = parts.get(field);
-  // This will notcompile
-  result.expect("oops! something went wrong").to_string()
+fn sum (numbers: &[i32]) -> i32 {
+  let mut result = 0;
+  for number in numbers {
+    result += number;
+  }
+  result
 }
 
 fn main () {
-  let chunk = split_string("hello,world".to_string(), ',', 1);
-  println!("Split string: {}", chunk);
+    // There are no variadic arguments in Rust
+    let numbers = [1, 2, 3, 4, 5];
+    let result = sum(&numbers);
+    println!("The sum is {}", result);
 }
-
-// The return value()
